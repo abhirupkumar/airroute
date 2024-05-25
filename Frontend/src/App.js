4daac74ccd575c7dba90e21140596b7bf7cfae11
Abhirup Kumar Bhowmick <94789673+abhirupkumar@users.noreply.github.com>
2024-05-25 16:05:59 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -25,7 +25,7 @@ export default function App() {
 
   const handleRouteSubmit = async (start, end) => {
     setLoading(true);
-    const response = await fetch(`https://airnavigation.onrender.com/shortest_path?start=${start.id}&end=${end.id}&prev`, {
+    const response = await fetch(`https://f91d-27-131-211-122.ngrok-free.app/shortest_path?start=${start.id}&end=${end.id}&prev`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
